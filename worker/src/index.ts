4d7221dd56ac472519d05c0727eae851691e39d5
hiverkiya <hiverkiya@gmail.com>
2025-08-02 22:51:45 +0530

    Implementing Kafka logic in worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -9,12 +9,20 @@ async function main() {
   await consumer.connect();
   await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
   await consumer.run({
+    autoCommit: false,
     eachMessage: async ({ topic, partition, message }) => {
       console.log({
         partition,
         offset: message.offset,
         value: message.value?.toString(),
       });
+      await consumer.commitOffsets([
+        {
+          topic: TOPIC_NAME,
+          partition,
+          offset: (parseInt(message.offset) + 1).toString(),
+        },
+      ]);
     },
   });
 }
