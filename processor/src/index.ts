eb9dc4ca5c68c5623aaaaa67dcc35582132b83e0
hiverkiya <hiverkiya@gmail.com>
2025-08-02 13:48:17 +0530

    Implementing Kafka with zap-events event

---
+++ processor/src/index.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+import { Kafka } from 'kafkajs';
+const client = new PrismaClient();
+const kafka = new Kafka({
+  clientId: 'outbox-processor',
+  brokers: ['localhost:9092'],
+});
+const TOPIC_NAME = 'zap-events';
+async function main() {
+  const producer = kafka.producer();
+  await producer.connect();
+  while (true) {
+    const pendingRows = await client.zapRunOutBox.findMany({
+      where: {},
+      take: 10,
+    });
+    producer.send({
+      topic: TOPIC_NAME,
+      messages: pendingRows.map((row) => ({
+        value: row.zapRunId,
+      })),
+    });
+  }
+}
+main();
