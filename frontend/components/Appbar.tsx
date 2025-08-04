932320b7d657fb4320ef2945eacf2c631d43e90b
hiverkiya <hiverkiya@gmail.com>
2025-08-04 09:12:12 +0530

    Frontend Pending

---
+++ frontend/components/Appbar.tsx
@@ -0,0 +1,10 @@
+export const Appbar = () => {
+  return (
+    <div className="flex border-b justify-between">
+      <div>Zapier</div>
+    </div>
+    <div>
+      <div className='font-small'></div>
+    </div>
+  );
+};
